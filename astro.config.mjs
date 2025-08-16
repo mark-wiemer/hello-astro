63a36569e1d88074cd46b1b09ed27e7a9d801988
Mark Wiemer <markwiemer@outlook.com>
2025-08-16 13:21:36 -0700

    Remove content until rendering kinda works
    First load fails, refresh always succeeds, error still logged

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
 import { defineConfig } from "astro/config";
-import starlight from "@astrojs/starlight";
 import { visit } from "unist-util-visit";
+import mdx from "@astrojs/mdx";
 
 // Relative links work locally but not in production
 const rewriteLinks = (base) => {
@@ -18,31 +18,8 @@ const base = "/next";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    starlight({
-      title: "My Docs",
-      social: [
-        {
-          icon: "github",
-          label: "GitHub",
-          href: "https://github.com/withastro/starlight",
-        },
-      ],
-      sidebar: [
-        {
-          label: "Guides",
-          items: [
-            // Each item here is one entry in the navigation menu.
-            { label: "Example Guide", slug: "guides/example" },
-          ],
-        },
-        {
-          label: "Reference",
-          autogenerate: { directory: "reference" },
-        },
-      ],
-    }),
-  ],
+  base,
+  integrations: [mdx()],
   markdown: {
     remarkPlugins: [rewriteLinks(base)],
   },
